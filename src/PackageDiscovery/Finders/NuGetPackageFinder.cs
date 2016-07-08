023692058b203a897101107adffd521a333d6a31
Péter Juhász <peter.juhasz@logmein.com>
2016-07-08 16:50:25 +0200

    Enumerate packages.config file from repositories.config files.

--- src/PackageDiscovery/Finders/NuGetPackageFinder.cs
+++ src/PackageDiscovery/Finders/NuGetPackageFinder.cs
@@ -11,8 +11,18 @@ namespace PackageDiscovery.Finders
 
         public IReadOnlyCollection<Package> FindPackages(DirectoryInfo directory)
         {
-            return directory
-                .GetFiles("packages.config", SearchOption.AllDirectories)
+            var packagesFromRepositories = (
+                from f in directory
+                    .GetFiles("repositories.config", SearchOption.AllDirectories)
+                let x = XDocument.Load(f.FullName)
+                from r in x.Root.Elements("repository")
+                select new FileInfo(Path.Combine(f.Directory.FullName, r.Attribute("path").Value))
+            );
+
+            var standalonePackages = directory
+                .GetFiles("packages.config", SearchOption.AllDirectories);
+
+            return standalonePackages.Union(packagesFromRepositories)
                 .Select(f => XDocument.Load(f.FullName))
                 .SelectMany(x => x.Root.Elements("package"))
                 .Select(x => new Package(Moniker, x.Attribute("id").Value, x.Attribute("version").Value))

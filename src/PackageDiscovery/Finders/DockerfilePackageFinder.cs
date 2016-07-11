d2a18c7cc30a12d78e49eb9bbd951641aac4320d
Péter Juhász <peter.juhasz@logmein.com>
2016-07-11 10:22:34 +0200

    Dockerfile support

---
+++ src/PackageDiscovery/Finders/DockerfilePackageFinder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PackageDiscovery.Finders
+{
+    [Export(Moniker, typeof(IReferencedPackageFinder))]
+    public sealed class DockerfilePackageFinder : IReferencedPackageFinder
+    {
+        public const string Moniker = "Docker";
+
+        private static readonly Regex PodRegex = new Regex(
+            @"\bFROM\s+(?<id>[^@\s]+)(@(?<version>[^\s]+))?"
+        );
+
+        public IReadOnlyCollection<Package> FindReferencedPackages(DirectoryInfo directory)
+        {
+            return directory
+                .GetFiles("Dockerfile", SearchOption.AllDirectories)
+                .Select(f => File.ReadAllText(f.FullName))
+                .SelectMany(c => PodRegex.Matches(c).Cast<Match>())
+                .Select(m => new Package(
+                    Moniker,
+                    m.Groups["id"].Value,
+                    m.Groups["version"]?.Value
+                ))
+                .Distinct(p => new { p.Id, p.Version })
+                .OrderBy(p => p.Id)
+                .ThenBy(p => p.Version)
+                .ToList();
+        }
+    }
+}
